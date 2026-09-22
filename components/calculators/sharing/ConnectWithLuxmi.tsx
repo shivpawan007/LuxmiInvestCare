e8595348beb04a763a1a23d43e818db7d5fce116
Shiv Pawan <shivpawan007@gmail.com>
2026-09-22 22:41:41 +0530

    Standardize projected-value and return terminology

--- components/calculators/sharing/ConnectWithLuxmi.tsx
+++ components/calculators/sharing/ConnectWithLuxmi.tsx
@@ -76,7 +76,7 @@ Report: ${reportTitle}
 
 Investment: ${formatINR(investment)}
 Investment Period: ${years} Years
-Expected Return Assumption: ${annualReturn}%
+Assumed Annual Return (Illustrative): ${annualReturn}%
 Illustrative Returns: ${formatINR(estimatedReturns)}
 Illustrative Projected Value: ${formatINR(maturityValue)}
 
